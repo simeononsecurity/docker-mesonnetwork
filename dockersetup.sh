c4a9f0a0b795e186068a8e15c4bfe1ff5dc57c68
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-11-24 13:40:53 -0600

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -5,4 +5,4 @@ tar -zxf meson_cdn-linux-amd64.tar.gz
 rm -f meson_cdn-linux-amd64.tar.gz 
 cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
 sudo ./meson_cdn config set --token="${token}" --https_port="${https_port}" --cache.size="${cache_size}"
-sudo ./service start meson_cdn
+sudo ./meson_cdn
